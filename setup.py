e26eb1b5004f281dcccce249724b9081816c1b0b
Indu <indumajji25@gmail.com>
2026-03-23 21:10:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="chatbot",
+    version="0.1.0",
+    author="indu",
+    author_email="indumajji25@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
